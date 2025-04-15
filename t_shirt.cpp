b882e018aafa66201b4bf6c792b2731915d8a971
bostonsinaga <bostonsinga@gmail.com>
2025-04-15 10:13:13 +0700

    Tidy up and change the output file path

--- t_shirt.cpp
+++ t_shirt.cpp
@@ -1,21 +1,22 @@
-/*
-*  g++ t-shirt.cpp
-*  a.exe catatan.txt C++ oleh Bjarne Stroustrup
-*/
+/**
+ * g++ t_shirt.cpp -o bin/baju
+ * bin/baju.exe catatan.txt C++ oleh Bjarne Stroustrup
+ */
 
 #include <iostream>
 #include <fstream>
 
 int main(int argc, char *argv[]) {
 
-    std::ofstream output(
-        argc > 1 ? argv[1] : "catatan_kosong.txt"
-    );
+  std::ofstream output(
+    argc > 1 ? argv[1] : "bin/catatan_kosong.txt"
+  );
 
-    for (int i = 2; i < argc; i++) {
-        output << argv[i] << (i < argc - 1 ? " " : "");
-    }
+  for (int i = 2; i < argc; i++) {
+    output << argv[i] << (i < argc - 1 ? " " : "");
+  }
 
-    output.close();
-    return 0;
+  output.close();
+  return 0;
 }
+
