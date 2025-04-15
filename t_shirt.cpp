10c4ec23326007ebfd4bb9d05a19d0e78587f2be
bostonsinaga <bostonsinga@gmail.com>
2025-04-15 11:01:08 +0700

    Last word from CLI is always included outside the loop

--- t_shirt.cpp
+++ t_shirt.cpp
@@ -1,6 +1,6 @@
 /**
  * compile.cmd
- * "bin/baju.exe" catatan.txt "C++ oleh Bjarne Stroustrup"
+ * "bin/baju.exe" catatan.txt C++ oleh Bjarne Stroustrup
  */
 
 #include <iostream>
@@ -12,10 +12,11 @@ int main(int argc, char *argv[]) {
     argc > 1 ? argv[1] : "catatan_kosong.txt"
   ));
 
-  for (int i = 2; i < argc; i++) {
-    output << argv[i] << (i < argc - 1 ? " " : "");
+  for (int i = 2; i < argc - 1; i++) {
+    output << argv[i] << ' ';
   }
 
+  output << argv[argc - 1];
   output.close();
   return 0;
 }
